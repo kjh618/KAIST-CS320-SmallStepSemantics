b6a27a5b1e19591de3d289c6b814a57c5230b7d3
Jihoon Kim <kjh618@kaist.ac.kr>
2019-11-12 21:29:51 +0900

    Add basic web interface

--- src/main/scala/smallstepsemantics/Main.scala
+++ src/main/scala/smallstepsemantics/Main.scala
@@ -1,6 +1,21 @@
 package smallstepsemantics
 
+import org.scalajs.dom
+import dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 object Main {
+  private val programInput = document.getElementById("programInput").asInstanceOf[dom.html.TextArea]
+  private val output = document.getElementById("output").asInstanceOf[dom.html.TextArea]
+
+  @JSExportTopLevel("runProgram")
+  def runProgram(): Unit = {
+    output.value = try Interpreter.run(programInput.value) catch {
+      case e: RuntimeException => e.getMessage
+      case e: Throwable => s"unknown error: $e"
+    }
+  }
+
   def main(args: Array[String]): Unit = {
     val program =
       """{withcc done
